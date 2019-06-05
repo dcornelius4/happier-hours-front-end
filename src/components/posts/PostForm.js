00e09a8bb3aa72146b5a8b34d29b89cacffaf3b8
dcornelius4 <d_cornelius94@yahoo.com>
2019-06-05 13:21:49 -0700

    created post form component

---
+++ src/components/posts/PostForm.js
@@ -0,0 +1,39 @@
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+
+export default class PostForm extends PureComponent {
+  static propTypes = {
+    onSubmit: PropTypes.func.isRequired
+  }
+
+  state = {
+    search: '',
+    times: '',
+    specials: ''
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+
+    const { search, times, specials } = this.state;
+    this.props.onSubmit(search, times, specials);
+    this.setState({ search: '', times: '', specials: '' });
+  }
+
+  handleChange = ({ target }) => {
+    this.setState({ [target.name]: target.value });
+  }
+
+  render() {
+    const { search, times, specials } = this.state;
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input name="search" value={search} onChange={this.handleChange} />
+        <input name="times" value={times} onChange={this.handleChange} />
+        <input name="specials" value={specials} onChange={this.handleChange} />
+        <button>Submit</button>
+      </form>
+    );
+  }
+}
+
