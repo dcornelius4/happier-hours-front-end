77934608566ae86f134fbdedf354d53a1139be9e
dcornelius4 <d_cornelius94@yahoo.com>
2019-06-06 16:50:08 -0700

    created search component

---
+++ src/components/search/Search.js
@@ -0,0 +1,44 @@
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+import styles from './Search.css';
+
+export default class Search extends PureComponent {
+  static propTypes = {
+    onSubmit: PropTypes.func.isRequired
+  }
+
+  state = {
+    search: '',
+    filter: ''
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+
+    const { search, filter } = this.state;
+    this.props.onSubmit(search,  filter);
+    this.setState({ search: '', filter: '' });
+  }
+
+  handleChange = ({ target }) => {
+    this.setState({ [target.name]: target.value });
+  }
+
+  render() {
+    const { search, filter, favorites, popular, happeningNow, surpriseMe, nearMe } = this.state;
+    return (
+      <form onSubmit={this.handleSubmit} className={styles.Form}>
+        <input name="search" value={search} onChange={this.handleChange} />
+        <select name="filter" value={filter} onChange={this.handleChange}>
+          <option>--Please Choose a filter--</option>
+          <option name="popular" value={popular}>Popular</option>
+          <option name="happeningNow" value={happeningNow}>Happening Now</option>
+          <option name="nearMe" value={nearMe}>Near Me</option>
+          <option name="surpriseMe" value={surpriseMe}>Surprise Me</option>
+          <option name="favorites" value={favorites}>Favorites</option>
+        </select>
+        <button>Submit</button>
+      </form>
+    );
+  }
+}
